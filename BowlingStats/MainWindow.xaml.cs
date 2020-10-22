1ab22fe6ff576eb1cef07e95d434f877b17abde1
Dana Wilson <danalwil@outlook.com>
2020-10-22 10:15:23 -0400

    2 Code Systems done

--- BowlingStats/MainWindow.xaml.cs
+++ BowlingStats/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace BowlingStats
     /// </summary>
     public partial class MainWindow : Window
     {
+        string operation = "";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,67 +30,174 @@ namespace BowlingStats
 
         private void btn0_Click(object sender, RoutedEventArgs e)
         {
-
+            if (operation == "")
+            {
+                Scoring1.Text = "0";
+            }
+            else
+            {
+                Scoring1.Text = "";
+            }
         }
 
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
-
+            if (operation == "")
+            {
+                Scoring1.Text = "1";
+            }
+            else
+            {
+                Scoring1.Text = "";
+            }
         }
 
         private void btn2_Click(object sender, RoutedEventArgs e)
         {
-
+            if (operation == "")
+            {
+                Scoring1.Text = "2";
+            }
+            else
+            {
+                Scoring1.Text = "";
+            }
         }
 
         private void btn3_Click(object sender, RoutedEventArgs e)
         {
-
+            if (operation == "")
+            {
+                Scoring1.Text = "3";
+            }
+            else
+            {
+                Scoring1.Text = "";
+            }
         }
 
         private void btn4_Click(object sender, RoutedEventArgs e)
         {
-
+            if (operation == "")
+            {
+                Scoring1.Text = "4";
+            }
+            else
+            {
+                Scoring1.Text = "";
+            }
         }
 
         private void btn5_Click(object sender, RoutedEventArgs e)
         {
-
+            if (operation == "")
+            {
+                Scoring1.Text = "5";
+            }
+            else
+            {
+                Scoring1.Text = "";
+            }
         }
 
         private void btn6_Click(object sender, RoutedEventArgs e)
         {
-
+            if (operation == "")
+            {
+                Scoring1.Text = "6";
+            }
+            else
+            {
+                Scoring1.Text = "";
+            }
         }
 
         private void btn7_Click(object sender, RoutedEventArgs e)
         {
-
+            if (operation == "")
+            {
+                Scoring1.Text = "7";
+            }
+            else
+            {
+                Scoring1.Text = "";
+            }
         }
 
         private void btn8_Click(object sender, RoutedEventArgs e)
         {
-
+            if (operation == "")
+            {
+                Scoring1.Text = "8";
+            }
+            else
+            {
+                Scoring1.Text = "";
+            }
         }
 
         private void btn9_Click(object sender, RoutedEventArgs e)
         {
-
+            if (operation == "")
+            {
+                Scoring1.Text = "9";
+            }
+            else
+            {
+                Scoring1.Text = "";
+            }
         }
 
         private void btn10_Click(object sender, RoutedEventArgs e)
         {
-
+            if (operation == "")
+            {
+                Scoring1.Text = "10";
+            }
+            else
+            {
+                Scoring1.Text = "";
+            }
         }
 
-        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        private void ExitButton_Click(object sender, RoutedEventArgs e) //Done//
         {
             this.Close();
         }
 
-        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        private void ClearButton_Click(object sender, RoutedEventArgs e) //Done//
         {
-            
-        }
+            yourName.Text = "";
+            Scoring1.Text = "";
+            Scoring2.Text = "";
+            FinalScoreFrom1.Text = "";
+            Scoring3.Text = "";
+            Scoring4.Text = "";
+            FinalScoreFrom2.Text = "";
+            Scoring5.Text = "";
+            Scoring6.Text = "";
+            FinalScoreFrom3.Text = "";
+            Scoring7.Text = "";
+            Scoring8.Text = "";
+            FinalScoreFrom4.Text = "";
+            Scoring9.Text = "";
+            Scoring10.Text = "";
+            FinalScoreFrom5.Text = "";
+            Scoring11.Text = "";
+            Scoring12.Text = "";
+            FinalScoreFrom6.Text = "";
+            Scoring13.Text = "";
+            Scoring14.Text = "";
+            FinalScoreFrom7.Text = "";
+            Scoring15.Text = "";
+            Scoring16.Text = "";
+            FinalScoreFrom8.Text = "";
+            Scoring17.Text = "";
+            Scoring18.Text = "";
+            FinalScoreFrom9.Text = "";
+            Scoring19.Text = "";
+            Scoring20.Text = "";
+            FinalScoreFrom10.Text = "";
+        } 
     }
 }

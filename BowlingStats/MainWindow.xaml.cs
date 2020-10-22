769ee1ee22f43f4fd3c79734ff54308cb9df52f4
Dana Wilson <danalwil@outlook.com>
2020-10-22 09:54:10 -0400

    Working on System

--- BowlingStats/MainWindow.xaml.cs
+++ BowlingStats/MainWindow.xaml.cs
@@ -23,5 +23,72 @@ namespace BowlingStats
         {
             InitializeComponent();
         }
+
+
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn10_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
